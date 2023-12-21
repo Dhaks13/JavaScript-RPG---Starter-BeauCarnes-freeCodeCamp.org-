406a38ac219ac2dae6eb3c6bccf6cb97dcf164e7
Dhakshin A V <avdhakshin1354@gmail.com>
2023-12-21 17:31:20 +0530

    updated func - script.js

--- script.js
+++ script.js
@@ -25,4 +25,21 @@ button3.onclick = fightDragon;
 //func for button 1
 function goStore() {
     console.log("Going to Store...");
+    button1.innerText = "Buy 10 health (10 Gold)";
+    button2.innerText = "Buy a weapon (30 Gold)";
+    button3.innerText = "Go to town square";
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
+    text.innerText = "You entered the store..."
+}
+
+//func for button 2
+function goCave() {
+    console.log("Going to Cave...");
+}
+
+//func for button 3
+function fightDragon() {
+    console.log("Fighting Dragon...");
 }
